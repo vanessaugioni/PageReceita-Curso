e2adcf3ea45805fa878f8f5bf648704d5d7539c4
Vanessa Ugioni <vanessa.ugioni@agpr5.com>
2023-06-09 17:20:47 -0300

    versão 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import { Waffle } from "./assets/export";
-import { Toggle } from './components/toggle/index'
-
+import { Toggle } from "./components/toggle/index";
 
 export default function App() {
   return (
     <>
-    
-    <Toggle />
-
+      <Toggle />
+     
       <main>
         <header>
           <h1>Waffles Clássico</h1>
 
           <img src={Waffle} alt="imagem" />
           <div>
-          <figcaption>Receita de Waffle clássico</figcaption>
+            <figcaption>Receita de Waffle clássico</figcaption>
           </div>
         </header>
-      
 
-       <section>
-            <h2>INGREDIENTES</h2>
+        <section>
+          <h2>INGREDIENTES</h2>
 
-           <ul>
+          <ul>
             <li>2 xícaras(chá) de farinha de trigo</li>
             <li>1 colher(sopa) de açucar</li>
             <li>2 colheres(sopa) de fermento em pó</li>
@@ -32,39 +29,46 @@ export default function App() {
             <li>4 colheres(sopa) de manteiga sem sal derretida</li>
             <li>1 e 3/4 de xícara(chá) de leite</li>
             <li>1 colher(sopa) de essência de baunilha</li>
-           </ul>
+          </ul>
+        </section>
 
-       </section>
+        <section>
+          <h2>MODO DE PREPARO</h2>
+          <div>
+            <ol>
+              1. Peneire em um recipiente a farinha de trigo, o açúcar, o amido,
+              o fermento e o sal. Reserve.
+            </ol>
+            <ol>
+              2. Em outro recipiente misture os ovos batidos com o leite, a
+              manteiga derretida e a essência de baunilha.
+            </ol>
+            <ol>
+              3. Despeje sobre a mistura de farinha e rapidamente incorpore os
+              ingredientes.
+            </ol>
+            <ol>
+              4. Aqueça o aparelho para Waffles. Coloque uma concha rasa de
+              massa e espalhe até cobrir o molde do aparelho, feche a tampa e
+              deixe assar até a massa ficar bem dourada.
+            </ol>
+            <ol>
+              5. Retire com espátulas de silicone. Sirva com mel, frutas ou
+              geleia.
+            </ol>
+          </div>
+        </section>
 
-       <section>
+        <section>
+          <h2>INFORMAÇÕES ADICIONAIS</h2>
 
-        <h2>MODO DE PREPARO</h2>
-     <div>
-        <ol>1. Peneire em um recipiente a farinha de trigo, o açúcar, o amido, o fermento e o sal. Reserve.</ol>
-        <ol>2. Em outro recipiente misture os ovos batidos com o leite, a manteiga derretida e a essência de baunilha.</ol>
-        <ol>3. Despeje sobre a mistura de farinha e rapidamente incorpore os ingredientes.</ol>
-        <ol>4. Aqueça o aparelho para Waffles. Coloque uma concha rasa de massa e espalhe até cobrir o molde do aparelho, feche a tampa e deixe assar até a massa ficar bem dourada.</ol>
-        <ol>5. Retire com espátulas de silicone. Sirva com mel, frutas ou geleia.</ol>
-      </div>
-
-       </section>     
-      
-       <section>
-
-       <h2>INFORMAÇÕES ADICIONAIS</h2>
-
-       <p>Você poderá servir estes Waffles no café da manhã com geleia.</p>
-
-      </section>
-    
-      
+          <p>Você poderá servir estes Waffles no café da manhã com geleia.</p>
+        </section>
       </main>
-         <footer>
-          <p>Criado com 💜 por </p>
-          <a href="https://github.com/vanessaugioni">Vanessa </a>
-
-         </footer>
-
+      <footer>
+        <p>Criado com 💜 por </p>
+        <a href="https://github.com/vanessaugioni">Vanessa </a>
+      </footer>
     </>
   );
 }
--- src/components/toggle/index.jsx
+++ src/components/toggle/index.jsx
@@ -1,9 +1,22 @@
+import "./style.scss";
+import { useEffect, useState } from "react";
+
 export function Toggle() {
+  const [checked, setChecked] = useState(false);
+
+  function handleChecked() {
+    setChecked(!checked);
+  }
+
+  useEffect(()=> {
+    console.log(checked)
+  },[checked])
 
   return (
-      
-    <input type="checkbox" className="ToggleButton"/>
-
-  )
-
+    <input
+      type="checkbox"
+      className={`toggleButton ${checked ? "checked" : ""}`}
+      onClick={handleChecked}
+    />
+  );
 }
